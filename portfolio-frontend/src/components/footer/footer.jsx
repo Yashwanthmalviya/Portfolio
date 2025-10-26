ef9c465ef46cd94a123897dfe42a5da335f28f44
Shreyasjainr <shreyasj135@gmail.com>
2025-10-27 00:34:53 +0530

    footer

--- portfolio-frontend/src/components/footer/footer.jsx
+++ portfolio-frontend/src/components/footer/footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import LogoX from "../../assets/images/logo-x.svg";
+import LogoLi from "../../assets/images/logo-li.svg";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
+    <div className="footer-cont">
         <div className="footer-socials">
-            <div>Find me in:</div>
-            <a className="socials-logo" href=""><img src="../../images/logo-li.svg" alt="" /></a>
-            <a className="socials-logo" href=""><img src="../../images/logo-gh.svg" alt="" /></a>
+            <div >Find me in:</div>
+            <a className="socials-logo" href=""><img src={LogoX} alt="" /></a>
+            <a className="socials-logo" href=""><img src={LogoLi} alt="" /></a>
         </div>
     </div>
   );
