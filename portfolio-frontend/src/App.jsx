80718a863cecbb6d7c83c68cde33f84e17e829e6
Shreyasjainr <shreyasj135@gmail.com>
2025-10-27 00:14:02 +0530

    Applied stash@{0} changes and kept local edits

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -3,14 +3,20 @@ import { useState } from 'react'
 import './index.css';
 import Home from './pages/Home'
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/footer';
 
 function App() {
 
 
   return (
     <>
-      <Navbar />
-      <Home />
+
+      <div className='main-container'>
+        <Navbar />
+        <Home />
+        <Footer />
+      </div>
+
     </>
   )
 }
--- portfolio-frontend/src/components/footer/footer.jsx
+++ portfolio-frontend/src/components/footer/footer.jsx
@@ -1,2 +1,15 @@
 import React from "react";
 
+const Footer = () => {
+  return (
+    <div className="footer-container">
+        <div className="footer-socials">
+            <div>Find me in:</div>
+            <a className="socials-logo" href=""><img src="../../images/logo-li.svg" alt="" /></a>
+            <a className="socials-logo" href=""><img src="../../images/logo-gh.svg" alt="" /></a>
+        </div>
+    </div>
+  );
+};
+ 
+export default Footer;
