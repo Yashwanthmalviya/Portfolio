deff9a5a47ecf09395210c0d24666c38e74941c5
Shreyasjainr <shreyasj135@gmail.com>
2025-10-27 14:39:55 +0530

    added git in footer

--- portfolio-frontend/src/components/footer/footer.jsx
+++ portfolio-frontend/src/components/footer/footer.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import LogoX from "../../assets/images/logo-x.svg";
 import LogoLi from "../../assets/images/logo-li.svg";
+import LogoGit from "../../assets/images/logo-git.svg";
 import "./footer.css";
 
 const Footer = () => {
   return (
     <div className="footer-cont">
-        <div className="footer-socials">
-            <div >Find me in:</div>
-            <a className="socials-logo" href=""><img src={LogoX} alt="" /></a>
-            <a className="socials-logo" href=""><img src={LogoLi} alt="" /></a>
-        </div>
+      <div className="footer-socials">
+        <div className="footer-text">find me in:</div>
+        <a className="socials-logo" href="x.com"><img src={LogoX} alt="" /></a>
+        <a className="socials-logo" href="linkedin.com"><img src={LogoLi} alt="" /></a>
+      </div>
+      <div className="socials-logo" style={{ gap: '8px' }}>
+        <a className="footer-git" href="github.com/shreyasjainr">@shreyasjainr
+        </a>
+        <img src={LogoGit} alt="" />
+      </div>
     </div>
   );
 };
- 
+
 export default Footer;
